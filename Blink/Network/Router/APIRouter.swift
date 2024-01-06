a069b9feebc9b752bfd7869bf2063be5492cefdb
Chaewon Yoon <ycw0363@gmail.com>
2024-01-06 21:00:43 +0900

    feat: 라우터 프로토콜 설정

---
+++ Blink/Network/Router/APIRouter.swift
@@ -0,0 +1,23 @@
+//
+//  APIRouter.swift
+//  Blink
+//
+//  Created by Chaewon on 1/4/24.
+//
+
+import Foundation
+import Alamofire
+
+protocol APIRouter: URLRequestConvertible {
+    var baseURL: URL { get }
+    var path: String { get }
+    var header: HTTPHeaders { get }
+    var method: HTTPMethod { get }
+    var query: [String: String] { get }
+    func asURLRequest() throws -> URLRequest
+}
+
+enum BaseURL: String {
+    case user = "/v1/users"
+    case workspace = "/v1/workspaces"
+}
