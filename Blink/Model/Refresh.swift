748367de51f0761bfa7ef99a33515a46420d558d
Chaewon Yoon <ycw0363@gmail.com>
2024-01-14 00:56:15 +0900

    feat: getWorkspace 네트워크 구성

---
+++ Blink/Model/Refresh.swift
@@ -0,0 +1,12 @@
+//
+//  Refresh.swift
+//  Blink
+//
+//  Created by Chaewon on 1/13/24.
+//
+
+import Foundation
+
+struct RefreshResponse: Decodable {
+    let accessToken: String
+}
---
+++ Blink/Model/WorkspaceInfo.swift
@@ -0,0 +1,17 @@
+//
+//  WorkspaceInfo.swift
+//  Blink
+//
+//  Created by Chaewon on 1/14/24.
+//
+
+import Foundation
+
+struct WorkspaceInfoResponse: Decodable {
+    let workspace_id: Int //워크스페이스 아이디
+    let name: String //워크스페이스 이름
+    let description: String? //워크스페이스 설명
+    let thumbnail: String //워크스페이스 썸네일 url
+    let owner_id: Int //워크스페이스 생성자 계정 유저 아이디
+    let createdAt: String //워크스페이스 생성 날짜
+}
