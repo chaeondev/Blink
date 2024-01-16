16df765161622e002f36fe9eb154612cca3c6288
Chaewon Yoon <ycw0363@gmail.com>
2024-01-16 17:30:45 +0900

    feat: tabbar view 샘플

---
+++ Blink/View/Search/SearchViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SearchViewController.swift
+//  Blink
+//
+//  Created by Chaewon on 1/16/24.
+//
+
+import UIKit
+
+final class SearchViewController: BaseViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .blue
+    }
+}
---
+++ Blink/View/Setting/SettingViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SettingViewController.swift
+//  Blink
+//
+//  Created by Chaewon on 1/16/24.
+//
+
+import UIKit
+
+final class SettingViewController: BaseViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .green
+    }
+}
