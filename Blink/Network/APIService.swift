e80d308b7c50fe33bbc28c07ab18fd6a3cfa3a51
Chaewon Yoon <ycw0363@gmail.com>
2024-01-17 21:43:45 +0900

    feat: multipart 네트워크 메서드 추가

--- Blink/Network/APIService.swift
+++ Blink/Network/APIService.swift
@@ -20,6 +20,8 @@ final class APIService {
     
     private init() { }
     
+    
+    // MARK: Single
     func request<T: Decodable, U: APIRouter>(type: T.Type, api: U) -> Single<NetworkResult<T>> {
         
         return Single<NetworkResult<T>>.create { single in
@@ -30,18 +32,15 @@ final class APIService {
                 case .success(let data):
                     print("===Network 통신 성공===")
                     return single(.success(.success(data)))
-                case .failure(let error):
-                    print("===Network 통신 실패=== \(response.response?.statusCode)")
+                case .failure(_):
+                    print("===Network 통신 실패=== \(String(describing: response.response?.statusCode))")
+                    
                     var errorResponse = ErrorResponse(errorCode: "server error")
                     
                     guard let data = response.data else { return single(.success(.failure(errorResponse))) }
-                    do {
-                        let serverError = try JSONDecoder().decode(ErrorResponse.self, from: data)
-                        errorResponse = serverError
-                    }
-                    catch {
-                        print(error)
-                    }
+                    
+                    errorResponse = self.decodingError(from: data)
+                    
                     print("===error code===", errorResponse)
                     return single(.success(.failure(errorResponse)))
                 }
@@ -52,6 +51,7 @@ final class APIService {
         
     }
     
+    // MARK: interceptor 포함 X 버전
     func requestNoInterceptor<T: Decodable, U: APIRouter>(type: T.Type, api: U) -> Single<NetworkResult<T>> {
         
         return Single<NetworkResult<T>>.create { single in
@@ -62,18 +62,15 @@ final class APIService {
                 case .success(let data):
                     print("===Network 통신 성공===")
                     return single(.success(.success(data)))
-                case .failure(let error):
-                    print("===Network 통신 실패=== \(response.response?.statusCode)")
+                case .failure(_):
+                    print("===Network 통신 실패=== \(String(describing: response.response?.statusCode))")
+                    
                     var errorResponse = ErrorResponse(errorCode: "server error")
                     
                     guard let data = response.data else { return single(.success(.failure(errorResponse))) }
-                    do {
-                        let serverError = try JSONDecoder().decode(ErrorResponse.self, from: data)
-                        errorResponse = serverError
-                    }
-                    catch {
-                        print(error)
-                    }
+                    
+                    errorResponse = self.decodingError(from: data)
+                    
                     print("===error code===", errorResponse)
                     return single(.success(.failure(errorResponse)))
                 }
@@ -85,7 +82,7 @@ final class APIService {
     }
 
     
-    
+    // MARK: Single+Empty Data -> Status code로 구분
     func requestEmptyReesponse<U: APIRouter>(api: U) -> Single<NetworkResult<String>> {
     
         return Single<NetworkResult<String>>.create { single in
@@ -101,18 +98,14 @@ final class APIService {
                     return single(.success(.success("Success")))
                     
                 } else {
-                    print("===Network 통신 실패=== \(response.response?.statusCode)")
+                    print("===Network 통신 실패=== \(String(describing: response.response?.statusCode))")
                     
                     var errorResponse = ErrorResponse(errorCode: "server error")
                     
                     guard let data = response.data else { return single(.success(.failure(errorResponse))) }
-                    do {
-                        let serverError = try JSONDecoder().decode(ErrorResponse.self, from: data)
-                        errorResponse = serverError
-                    }
-                    catch {
-                        print(error)
-                    }
+                    
+                    errorResponse = self.decodingError(from: data)
+                    
                     print("===error code===", errorResponse)
                     return single(.success(.failure(errorResponse)))
                 }
@@ -121,8 +114,50 @@ final class APIService {
         }
     }
     
+    // MARK: Single + Multipart
+    func requestMultipart<T: Decodable, U: APIRouter>(type: T.Type, api: U) -> Single<NetworkResult<T>> {
+        
+        return Single<NetworkResult<T>>.create { single in
+            
+            AF.upload(multipartFormData: api.multipart, with: api, interceptor: AuthInterceptor())
+                .validate()
+                .responseDecodable(of: T.self) { response in
+                
+                switch response.result {
+                case .success(let data):
+                    print("===Multipart - Network 통신 성공===")
+                    return single(.success(.success(data)))
+                case .failure(_):
+                    print("===Multipart - Network 통신 실패=== \(String(describing: response.response?.statusCode))")
+                    
+                    var errorResponse = ErrorResponse(errorCode: "server error")
+                    
+                    guard let data = response.data else { return single(.success(.failure(errorResponse))) }
+                    
+                    errorResponse = self.decodingError(from: data)
+                    
+                    print("===error code===", errorResponse)
+                    return single(.success(.failure(errorResponse)))
+                }
+            }
+            return Disposables.create()
+            
+        }
+        
+    }
     
-    
+    private func decodingError(from jsonData: Data) -> ErrorResponse {
+        do {
+            let serverError = try JSONDecoder().decode(ErrorResponse.self, from: jsonData)
+            
+            return serverError
+        }
+        catch {
+            print(error)
+            
+            return ErrorResponse(errorCode: "server error")
+        }
+    }
     
     
     
