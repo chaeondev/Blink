8ca342504acb0b46e35cb2019ae231eb51eaafa7
Chaewon Yoon <ycw0363@gmail.com>
2024-01-18 17:20:08 +0900

    feat: 워크스페이스 생성 네트워크 통신 구현

--- Blink/View/Workspace/WSAdd/WSAddViewController.swift
+++ Blink/View/Workspace/WSAdd/WSAddViewController.swift
@@ -15,8 +15,6 @@ final class WSAddViewController: BaseViewController {
     private let viewModel = WSAddViewModel()
     private var disposeBag = DisposeBag()
     
-    let profileImage = PublishSubject<UIImage>()
-    
     override func loadView() {
         self.view = mainView
     }
@@ -30,7 +28,6 @@ final class WSAddViewController: BaseViewController {
     
     private func bind() {
         let input = WSAddViewModel.Input(
-            imageData: profileImage.asObservable().map { $0.jpegData(compressionQuality: 0.2) },
             nameText: mainView.titleTextField.rx.text.orEmpty,
             descriptText: mainView.infoTextField.rx.text.orEmpty,
             doneButtonTap: mainView.doneButton.rx.tap
@@ -43,8 +40,59 @@ final class WSAddViewController: BaseViewController {
                 owner.showActionSheet()
             }
             .disposed(by: disposeBag)
+        
+        // MARK: 생성 버튼 활성화
+        input.nameText
+            .map { !$0.isEmpty }
+            .bind(with: self) { owner, bool in
+                owner.mainView.doneButton.rx.isEnabled.onNext(bool)
+                owner.mainView.doneButton.backgroundColor = bool ? .brandGreen : .brandInactive
+            }
+            .disposed(by: disposeBag)
+        
+        // MARK: validation 따른 토스트 메세지
+        output.validationOutput
+            .bind(with: self) { owner, validation in
+                var message: String {
+                    switch validation {
+                    case .invalidName:
+                        return "워크스페이스 이름은 1~30자로 설정해주세요."
+                    case .invalidImage:
+                        return "워크스페이스 이미지를 등록해주세요."
+                    }
+                }
+                
+                let toastPosition = owner.mainView.doneButton.frame.origin.y - 30
+                owner.toast(message: message, pointY: toastPosition)
+            }
+            .disposed(by: disposeBag)
+        
+        // MARK: 네트워크 결과
+        output.createWSResult
+            .bind(with: self) { owner, result in
+                
+                let toastPosition = owner.mainView.doneButton.frame.origin.y - 30
+                
+                switch result {
+                case .success(let response):
+                    let wsID = response.workspace_id
+                    
+                    let vc = HomeTabViewController()
+                    vc.workspaceID = wsID
+                    
+                    owner.changeRootViewController(viewController: vc)
+                case .networkError:
+                    owner.toast(message: "에러가 발생했어요. 잠시 후 다시 시도해주세요.", pointY: toastPosition)
+                case .serverConflict:
+                    owner.toast(message: "중복된 이름입니다. 워크스페이스 이름은 고유한 이름으로 지정해주세요", pointY: toastPosition)
+                case .insufficientCoin:
+                    owner.toast(message: "새싹 코인이 부족합니다. 코인을 채워주세요", pointY: toastPosition)
+                }
+            }
+            .disposed(by: disposeBag)
     }
     
+    // TODO: 나중에 extension으로 빼기
     private func showActionSheet() {
         let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
         let cameraAction = UIAlertAction(title: "카메라", style: .default) { (action) in
@@ -88,7 +136,11 @@ extension WSAddViewController: UIImagePickerControllerDelegate, UINavigationCont
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         
         if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
-            profileImage.onNext(image)
+            
+            guard let imageData = image.jpegData(compressionQuality: 0.01) else { return }
+            
+            self.viewModel.imageData.onNext(imageData)
+
             mainView.profileButton.setImage(image, for: .normal)
             mainView.profileButton.contentMode = .scaleAspectFill
             mainView.profileButton.imageEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
--- Blink/View/Workspace/WSAdd/WSAddViewModel.swift
+++ Blink/View/Workspace/WSAdd/WSAddViewModel.swift
@@ -11,28 +11,122 @@ import RxCocoa
 
 final class WSAddViewModel: ViewModelType {
     
+    enum CreateWorkspaceValidation {
+        case invalidName
+        case invalidImage
+    }
+    
+    enum CreateWSResult<T: Decodable> {
+        case networkError
+        case serverConflict
+        case insufficientCoin
+        case success(T)
+    }
+    
+    let imageData = PublishSubject<Data>()
+    
     private var disposeBag = DisposeBag()
     
     struct Input {
-        let imageData: Observable<Data?>
         let nameText: ControlProperty<String>
         let descriptText: ControlProperty<String>
         let doneButtonTap: ControlEvent<Void>
     }
     
     struct Output {
-        
+        let validationOutput: PublishSubject<CreateWorkspaceValidation>
+        let createWSResult: PublishSubject<CreateWSResult<WorkspaceInfoResponse>>
     }
     
     func transform(input: Input) -> Output {
         
         let isValidName = BehaviorSubject(value: false)
+        let isValidImage = BehaviorSubject(value: false)
+        
+        let validationOutput = PublishSubject<CreateWorkspaceValidation>()
+        let createWSResult = PublishSubject<CreateWSResult<WorkspaceInfoResponse>>()
         
         //워크스페이스 이름 검증
         input.nameText
             .map { $0.count >= 1 && $0.count <= 30 }
             .bind(to: isValidName)
             .disposed(by: disposeBag)
-        return Output()
+        
+        //워크스페이스 이미지 검증
+        imageData
+            .subscribe(with: self) { owner, value in
+                isValidImage.onNext(!value.isEmpty)
+            }
+            .disposed(by: disposeBag)
+        
+        // MARK: 생성(완료)버튼 클릭
+        
+        //request 구조체 만들기
+        let createRequestModel = Observable.combineLatest(
+            input.nameText,
+            input.descriptText,
+            imageData
+        )
+            .map { (name, descript, image) in
+                return WorkspaceInfoRequest(
+                    name: name,
+                    description: descript,
+                    image: image
+                )
+            }
+        
+        let validation = Observable.combineLatest(
+            isValidName,
+            isValidImage
+        )
+        
+        //생성 버튼 클릭
+        input.doneButtonTap
+            .throttle(.seconds(1), scheduler: MainScheduler.instance)
+            .withLatestFrom(validation)
+            .filter { value in
+                if !value.0 {
+                    validationOutput.onNext(.invalidName)
+                    return false
+                } else if !value.1 {
+                    validationOutput.onNext(.invalidImage)
+                    return false
+                } else {
+                    return true
+                }
+                print("====걸리면 안됨===")
+            }
+            .withLatestFrom(createRequestModel)
+            .flatMapLatest {
+                APIService.shared.requestMultipart(type: WorkspaceInfoResponse.self, api: WorkspaceRouter.createWorkspace($0))
+            }
+            .subscribe(with: self) { owner, result in
+                switch result {
+                case .success(let response):
+                    print("===Create Workspace success===")
+                    createWSResult.onNext(.success(response))
+                case .failure(let error):
+                    let isCommonError = NetworkError.allCases.map { $0.rawValue }.contains(error.errorCode)
+                    let customError: (any HTTPError)? = isCommonError ? NetworkError(rawValue: error.errorCode) : CreateWorkspaceError(rawValue: error.errorCode)
+                    
+                    if let wsError = customError as? CreateWorkspaceError {
+                        switch wsError {
+                        case .wrongRequest:
+                            createWSResult.onNext(.networkError)
+                        case .serverConflict:
+                            createWSResult.onNext(.serverConflict)
+                        case .insufficientCoin:
+                            createWSResult.onNext(.insufficientCoin)
+                        }
+                    }
+                    
+                    print("==Create Workspace Failed \(String(describing: customError))")
+                }
+            }
+            .disposed(by: disposeBag)
+        
+        
+        
+        return Output(validationOutput: validationOutput, createWSResult: createWSResult)
     }
 }
