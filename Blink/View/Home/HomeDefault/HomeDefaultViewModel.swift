35578fef94ecf3fbdfb449de980b4fae7a312629
Chaewon Yoon <ycw0363@gmail.com>
2024-01-23 10:36:37 +0900

    feat: tableView 데이터 네트워크 메서드 구현(무한루프 fix, rx대신 completion사용)

--- Blink/View/Home/HomeDefault/HomeDefaultViewModel.swift
+++ Blink/View/Home/HomeDefault/HomeDefaultViewModel.swift
@@ -38,11 +38,11 @@ final class HomeDefaultViewModel: ViewModelType {
     private var disposeBag = DisposeBag()
     
     //TableView Data
-    var channelData = BehaviorRelay<HomeChannelsData>(value: HomeChannelsData(opened: false, sectionData: []))
-    var dmData = BehaviorRelay<HomeDMsData>(value: HomeDMsData(opened: false, sectionData: []))
+    var channelData: HomeChannelsData?
+    var dmData: HomeDMsData?
     
     struct Input {
-
+        
     }
     
     struct Output {
@@ -83,81 +83,105 @@ final class HomeDefaultViewModel: ViewModelType {
             }
             .disposed(by: disposeBag)
         
-       
+        
         
         return Output(workspaceResource: workspaceResource, profileResource: profileResource)
     }
     
-    //bind에 넣기에는 reload table을 또 어떻게 전달해야할 지 모르겠음 그럴바에는 메서드로 빼자
-    func fetchChannelTableData() {
-        // MARK: 채널 정보조회
-        APIService.shared.request(type: [ChannelRes].self, api: ChannelRouter.checkMyChannels(self.workspaceID))
-            .subscribe(with: self) { owner, result in
-                switch result {
-                    
-                case .success(let response):
-                    print("===내 채널 정보 조회 성공===")
-                    let data = HomeChannelsData(
-                        opened: true,
-                        sectionData: response.map {
-                            HomeChannelsData.ChannelCellInfo(channelInfo: $0, unreadChatCnt: 10)
-                        }
-                    )
-                    
-                    self.channelData.accept(data)
-                    
-                case .failure(let error):
-                    
-                    print("===내 채널 정보 조회 실패 -> 채팅갯수 네트워크 연결 안함===")
-                    print(error)
-                    
-                    //sectionData가 빈배열인 것을 전달 -> failure인것 파악 가능
-                    let data = HomeChannelsData(opened: true, sectionData: [])
-                    self.channelData.accept(data)
-                }
-            }
-            .disposed(by: disposeBag)
-        
-        // MARK: 읽지 않은 채팅 갯수 조회
-        
-        //구조체 만들기
-        self.channelData
-            .filter { !$0.sectionData.isEmpty } // 채널 정보 조회 실패시 request 구조체 자체를 안만들어 버림
-            .map {
-                for (index, item) in $0.sectionData.enumerated() {
-                    let requestModel = ChannelUnreadCountRequest(
-                        workspaceID: item.channelInfo.workspace_id,
-                        channelName: item.channelInfo.name,
-                        after: item.channelInfo.createdAt // TODO: 현재는 생성 시점부터 읽지않은걸로 처리!! 꼭꼭 바꾸기!!
-                    )
-                    
-                    //네트워크 통신
-                    APIService.shared.request(type: ChannelUnreadCountResponse.self, api: ChannelRouter.channelChatUnreadCount(requestModel))
-                        .subscribe(with: self) { owner, result in
-                            switch result {
-                            case .success(let response):
-                                print("===\(response.name) 채널 읽지 않은 채팅 개수: \(response.count) 네트워크 성공===")
-                                var value = owner.channelData.value
-                                value.sectionData[index].unreadChatCnt = response.count
-                                
-                                owner.channelData.accept(value)
-                            case .failure(let error):
-                                var value = owner.channelData.value
-                                print("===채널 읽지 않은 채팅 개수 네트워크 실패===")
-                                print("===채널 정보: \(value.sectionData[index].channelInfo)===")
-                                print("===에러: \(error)===")
-                            }
-                        }
-                        .disposed(by: self.disposeBag)
-                    
-                }
+    
+    func fetchChannelInfo(completion: @escaping () -> Void) {
+        APIService.shared.requestCompletion(type: [ChannelRes].self, api: ChannelRouter.checkMyChannels(self.workspaceID)) { [weak self] result in
+            switch result {
+            case .success(let response):
+                print("===내 채널 정보 조회 성공===")
+                self?.channelData = HomeChannelsData(
+                    opened: true,
+                    sectionData: response.map {
+                        HomeChannelsData.ChannelCellInfo(channelInfo: $0, unreadChatCnt: 10)
+                    }
+                )
                 
-                return $0
+                self?.fetchChannelUnreadCount {
+                    completion()
+                }
+            case .failure(let error):
+                print("===내 채널 정보 조회 실패 -> 채팅갯수 네트워크 연결 안함===")
+                print(error)
             }
-            .subscribe(with: self) { owner, data in
-                print("===channelData===", data)
-            }
-            .disposed(by: disposeBag)
-      
+        }
     }
+    
+    func fetchChannelUnreadCount(completion: @escaping () -> Void) {
+        guard let channelData else { return }
+        
+        for (index, item) in channelData.sectionData.enumerated() {
+            let requestModel = ChannelUnreadCountRequest(
+                workspaceID: self.workspaceID,
+                channelName: item.channelInfo.name,
+                after: item.channelInfo.createdAt // TODO: 현재는 생성 시점부터 읽지않은걸로 처리!! 꼭꼭 바꾸기!!
+            )
+            
+            APIService.shared.requestCompletion(type: ChannelUnreadCountResponse.self, api: ChannelRouter.channelChatUnreadCount(requestModel)) { [weak self] result in
+                switch result {
+                case .success(let response):
+                    print("===\(response.name) 채널 읽지 않은 채팅 개수: \(response.count) 네트워크 성공===")
+                    self?.channelData!.sectionData[index].unreadChatCnt = response.count
+                    
+                    completion()
+                case .failure(let error):
+                    print("===채널 읽지 않은 채팅 개수 네트워크 실패===")
+                    print("===채널 정보: \(String(describing: self?.channelData?.sectionData[index].channelInfo))===")
+                    print("===에러: \(error)===")
+                }
+            }
+            
+        }
+    }
+    
+    func fetchDMInfo(completion: @escaping () -> Void) {
+        APIService.shared.requestCompletion(type: [DMsRoomRes].self, api: DMSRouter.checkDMRooms(self.workspaceID)) { [weak self] result in
+            switch result {
+            case .success(let response):
+                print("===DM 정보 조회 성공 ===")
+                self?.dmData = HomeDMsData(
+                    opened: true,
+                    sectionData: response.map {
+                        HomeDMsData.DMCellInfo(dmInfo: $0, unreadDMCnt: 10)
+                    }
+                )
+                
+            case .failure(let error):
+                print("===DM 정보 조회 실패 ===", error)
+            }
+        }
+    }
+    
+    func fetchDMsUnreadCount(completion: @escaping () -> Void) {
+        guard let dmData else { return }
+        
+        for (index, item) in dmData.sectionData.enumerated() {
+            let requestModel = DMsUnreadCountRequest(
+                roomID: item.dmInfo.room_id,
+                workspaceID: self.workspaceID,
+                after: item.dmInfo.createdAt) // TODO: 꼭 바꾸기!!!
+            
+            APIService.shared.requestCompletion(type: DMsUnreadCountResponse.self, api: DMSRouter.dmsUnreadCount(requestModel)) { [weak self] result in
+                switch result {
+                case .success(let response):
+                    print("===\(response.room_id) 방 DM개수: \(response.count) 네트워크 성공===")
+                    self?.dmData!.sectionData[index].unreadDMCnt = response.count
+                    
+                    completion()
+                case .failure(let error):
+                    print("===DM 읽지 않은 채팅 개수 네트워크 실패===")
+                    print("===DM방 정보: \(String(describing: self?.dmData?.sectionData[index].dmInfo))")
+                    print("===에러: \(error)===")
+                }
+            }
+        }
+    }
+    
+    
 }
+    
+    
